3eb0aa605035f755088ee88c6d605f0b92a7b8dc
Ming <mingdai.us@gmail.com>
2017-04-20 01:09:03 -0700

    Update DijstraShortestPath.cpp

--- Graph/DijstraShortestPath.cpp
+++ Graph/DijstraShortestPath.cpp
@@ -2,32 +2,32 @@
 // Graph is represented as adjacent list, Min HashHeap is used to improve timing complexity
 // Code of HashHeap is in the data structure directory
 
-vector<int>  dijstraShortestPath(vector<unordered_map<int, int>> &myGraph, int source) {
+vector<int> dijstraShortestPath(vector<unordered_map<int, int>> &myGraph, int source) {
     int size = myGraph.size();
-	vector<int> dist(size, INT_MAX);
+    vector<int> dist(size, INT_MAX);
     // initiate the minHeap with source node
-	HashHeap minHeap;
-	minHeap.insert(source, 0);
-	for (int i = 0; i < size; ++i) {
-		HeapNode cur = minHeap.getMin();
-		minHeap.popMin();
-		dist[cur.key] = cur.val;
-		for (pair<int, int> e : myGraph[cur.key]) {
-			int key = e.first;
-			// shortest path to key aleready found
-			if (dist[key] != INT_MAX) {
-				continue;
-			}
-			int val = cur.val + e.second;
-			// existing path is shorter
-			if(minHeap.getKey(key) <= val) {
-				continue;
-			}
+    HashHeap minHeap;
+    minHeap.insert(source, 0);
+    for (int i = 0; i < size; ++i) {
+        HeapNode cur = minHeap.getMin();
+        minHeap.popMin();
+        dist[cur.key] = cur.val;
+        for (pair<int, int> e : myGraph[cur.key]) {
+            int key = e.first;
+            // shortest path to key aleready found
+            if (dist[key] != INT_MAX) {
+                continue;
+            }
+            int val = cur.val + e.second;
+            // existing path is shorter
+            if(minHeap.getKey(key) <= val) {
+                continue;
+            }
             // update minHeap to include the shorter path
-			minHeap.remove(key);
-			minHeap.insert(key, val);
-		}
-	}
-	return dist;
+            minHeap.remove(key);
+            minHeap.insert(key, val);
+        }
+    }
+    return dist;
 }
 
