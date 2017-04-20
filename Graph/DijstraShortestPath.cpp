7caa9c16a7da98c6298c9b8105096414475c8b04
Ming <mingdai.us@gmail.com>
2017-04-19 22:49:33 -0700

    Update DijstraShortestPath.cpp

--- Graph/DijstraShortestPath.cpp
+++ Graph/DijstraShortestPath.cpp
@@ -1,1 +1,6 @@
-// Implementation of Dijstra's shortest path algorithm (graph should not contain negative edge)
+// Dijstra's shortest path algorithm (graph should not contain negative edge)
+// Graph is represented as adjacent list, Min Hashheap is used to improve timing complexity
+
+
+
+
