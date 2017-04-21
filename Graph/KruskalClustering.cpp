920f54fbac0323257f607da0913ecba9771c54c9
Ming <mingdai.us@gmail.com>
2017-04-20 23:39:36 -0700

    Create KruskalClustering.cpp

---
+++ Graph/KruskalClustering.cpp
@@ -0,0 +1,3 @@
+// Code of Kruskals' algorithm to solve k-clustering
+// k-clustering: group nodes into k clusters to maximize the minimum distance bewteen any 2 clusters
+// MST(minimum spanning tree) can be treated as 1-clustering problem
