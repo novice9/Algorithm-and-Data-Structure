a35f97accd6f86373e75df5cbd3d897178fd6607
Ming <mingdai.us@gmail.com>
2017-04-20 22:49:35 -0700

    Create SCC.cpp

---
+++ Graph/SCC.cpp
@@ -0,0 +1,8 @@
+// code of finding strong connected component with DFS
+
+vector<vector<int>> scc(vector<vector<int>> &graph) {
+    int noofnode = graph.size();
+    vector<int> tOrder(noofnode, -1);
+    int index(0);
+    
+}
