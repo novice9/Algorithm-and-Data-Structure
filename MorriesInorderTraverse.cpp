286c164f9006bb069336476996b79285246d8f58
Ming <mingdai.us@gmail.com>
2017-05-27 22:51:03 -0700

    Update MorriesInorderTraverse.cpp

--- MorriesInorderTraverse.cpp
+++ MorriesInorderTraverse.cpp
@@ -1,30 +1,30 @@
 // The code of morries algorithm to traverse binary search without extra space
 
 vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> trav;
-        if (root == NULL) {
-            return trav;
-        }
-        TreeNode* cur = root;
-        TreeNode* pre = NULL;
-        while (cur != NULL) {
-            if (cur->left != NULL) {
-                pre = cur->left;
-                while (pre != NULL && pre->right != NULL && pre->right != cur) {
-                    pre = pre->right;
-                }
-                if (pre->right == NULL) {
-                    pre->right = cur;
-                    cur = cur->left;
-                } else {
-                    pre->right = NULL;
-                    trav.push_back(cur->val);
-                    cur = cur->right;
-                }
-            } else {
-                trav.push_back(cur->val);
-                cur = cur->right;
-            }
-        }
-        return trav;
-    }
+	vector<int> trav;
+	if (root == NULL) {
+		return trav;
+	}
+	TreeNode* cur = root;
+	TreeNode* pre = NULL;
+	while (cur != NULL) {
+		if (cur->left != NULL) {
+			pre = cur->left;
+			while (pre != NULL && pre->right != NULL && pre->right != cur) {
+				pre = pre->right;
+			}
+			if (pre->right == NULL) {
+				pre->right = cur;
+				cur = cur->left;
+			} else {
+				pre->right = NULL;
+				trav.push_back(cur->val);
+				cur = cur->right;
+			}
+		} else {
+			trav.push_back(cur->val);
+			cur = cur->right;
+		}
+	}
+	return trav;
+}
