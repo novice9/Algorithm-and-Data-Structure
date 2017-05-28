4dc34f07793b27722e38fd70b8327f13fc0f14bf
Ming <mingdai.us@gmail.com>
2017-05-27 22:54:26 -0700

    Update MorriesInorderTraverse.cpp

--- MorriesInorderTraverse.cpp
+++ MorriesInorderTraverse.cpp
@@ -7,24 +7,24 @@ vector<int> inorderTraversal(TreeNode* root) {
     }
     TreeNode* cur = root;
     TreeNode* pre = NULL;
-	while (cur != NULL) {
-		if (cur->left != NULL) {
-			pre = cur->left;
-			while (pre != NULL && pre->right != NULL && pre->right != cur) {
-				pre = pre->right;
-			}
-			if (pre->right == NULL) {
-				pre->right = cur;
-				cur = cur->left;
-			} else {
-				pre->right = NULL;
-				trav.push_back(cur->val);
-				cur = cur->right;
-			}
-		} else {
-			trav.push_back(cur->val);
-			cur = cur->right;
-		}
-	}
-	return trav;
+    while (cur != NULL) {
+        if (cur->left != NULL) {
+            pre = cur->left;
+            while (pre != NULL && pre->right != NULL && pre->right != cur) {
+                pre = pre->right;
+            }
+            if (pre->right == NULL) {
+                pre->right = cur;
+                cur = cur->left;
+            } else {
+                pre->right = NULL;
+                trav.push_back(cur->val);
+                cur = cur->right;
+            }
+        } else {
+            trav.push_back(cur->val);
+            cur = cur->right;
+        }
+    }
+    return trav;
 }
