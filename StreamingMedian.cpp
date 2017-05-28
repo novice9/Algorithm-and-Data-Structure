a0c88db198ac6d8533fc245b6df8207d7efbc629
Ming <mingdai.us@gmail.com>
2017-05-27 23:54:28 -0700

    Create StreamingMedian.cpp

---
+++ StreamingMedian.cpp
@@ -0,0 +1,9 @@
+// code of calculate streaming medians with 2 heaps
+
+vector<int> streamingMedians(vector<int> &nums) {
+    priority_queue<int, vector<int>, greater<int>> upper;
+    priority_queue<int, vector<int>, less<int>> lower;
+    vector<int> medians;
+    for (int num : nums) {
+    }
+}
