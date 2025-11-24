8dc6099e3232f1c628058fb56c410e205ba3389e
ming <mingdai.us@gmail.com>
2025-11-23 17:20:59 -0800

    Resolve conflict

--- Tests/PermutationTest.cpp
+++ Tests/PermutationTest.cpp
@@ -2,20 +2,12 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
-<<<<<<< HEAD
-#include "IndexOfPermutation.cpp"
-=======
 #include "../IndexOfPermutation.cpp"
->>>>>>> cad425d (WIP)
 using namespace std;
 
 int main() {
   vector<int> input = {2,5,3,1,3};
   int index = permutationIndexII(input);
   cout << index << endl;
-<<<<<<< HEAD
-  return 1;    
-=======
   return 1;
->>>>>>> cad425d (WIP)
 }
